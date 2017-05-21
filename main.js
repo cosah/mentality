777649f7a79fd90cf239a1f3290457eaca0efe9c
cosah <anthonybshephard@gmail.com>
2017-05-21 07:39:51 -0400

    Add existing file

--- main.js
+++ main.js
@@ -12,7 +12,62 @@ window.onload = function () {
   score4avg = avgScore4(score4avg);
   scoreavg = roundToTwo((score1avg + score2avg + score3avg + score4avg)/4);
   teamScore();
+  academicTable();
+  sleepTable();
+  stressTable();
+  relaTable();
 };
+
+function academicTable(){
+  var academwrapper = document.getElementById("academicwrapper");
+  academwrapper.setAttribute("class", "col-md-5 " + bucketScore(score1avg));
+  var academics = document.getElementById("academics");
+  for(var stu in team){
+    var row = document.createElement("TR");                 // Create a <li> node
+    var lName = document.createElement("TD");         // Create a text node
+    var LName = document.createTextNode(team[stu].lastName);         // Create a text node
+    lName.appendChild(LName);
+    var fName = document.createElement("TD");
+    var FName = document.createTextNode(team[stu].firstName);
+    fName.appendChild(FName);
+    var eleScore = document.createElement("TD");
+    var ELEScore = document.createTextNode(team[stu].score1);
+    eleScore.appendChild(ELEScore);
+    row.appendChild(lName);                              // Append the text to <li>
+    row.appendChild(fName);
+    row.appendChild(eleScore);
+    academics.appendChild(row);
+  }
+}
+function sleepTable(){
+  var sleepwrapper = document.getElementById("sleepwrapper");
+  sleepwrapper.setAttribute("class", "col-md-5 " + bucketScore(score2avg));
+}
+function stressTable(){
+  var stresswrapper = document.getElementById("stresswrapper");
+  stresswrapper.setAttribute("class", "col-md-5 " + bucketScore(score3avg));
+  var stress = document.getElementById("stress");
+  for(var stu in team){
+    var row = document.createElement("TR");                 // Create a <li> node
+    var lName = document.createElement("TD");         // Create a text node
+    var LName = document.createTextNode(team[stu].lastName);         // Create a text node
+    lName.appendChild(LName);
+    var fName = document.createElement("TD");
+    var FName = document.createTextNode(team[stu].firstName);
+    fName.appendChild(FName);
+    var eleScore = document.createElement("TD");
+    var ELEScore = document.createTextNode(team[stu].score3);
+    eleScore.appendChild(ELEScore);
+    row.appendChild(lName);                              // Append the text to <li>
+    row.appendChild(fName);
+    row.appendChild(eleScore);
+    stress.appendChild(row);
+  }
+}
+function relaTable(){
+  var relawrapper = document.getElementById("relationshipswrapper");
+  relawrapper.setAttribute("class", "col-md-5 " + bucketScore(score4avg));
+}
 
 function roundToTwo(num) {
     return +(Math.round(num + "e+2")  + "e-2");
@@ -82,4 +137,16 @@ function teamScore(){
     document.getElementById("score").innerHTML='<span class="label label-danger" id="avgscore">Team Mentality Score</span>';
   }
   document.getElementById("avgscore").innerHTML= "Team Mentality Score: " + scoreavg;
+}
+
+function bucketScore(bucket){
+  if(bucket >= 4){
+    return "bg-success";
+  }else if(bucket >= 3){
+    return "bg-info";
+  }else if(bucket >= 2){
+    return "bg-warning";
+  }else{
+    return "bg-danger";
+  }
 }
