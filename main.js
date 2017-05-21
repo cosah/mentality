0186d8151a23aa93c22aaa215e90eaed47b2b4e1
cosah <anthonybshephard@gmail.com>
2017-05-21 02:38:49 -0400

    Add existing file

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
-function openReg() {
-  document.getElementById("reg").style.visibility = "visible";
+var team = [];
+window.onload = function () {
+  teamInit();
+};
+
+function student(first, last, dob, major, gradyr, sex) {
+    this.firstName = first;
+    this.lastName = last;
+    this.dob = dob;
+    this.major = major;
+    this.gradYear = gradyr;
+    this.gender = sex;
+}
+
+function teamInit(){
+  team.push(new student("Bill", "Wallace", "1995/05/05", "English", "2018", "Male"));
+  team.push(new student("Jeff", "Goldblum", "1997/11/07", "Paleontology", "2019", "Male"));
+  team.push(new student("Jerry", "Tom", "1996/12/25", "Civil Engineering", "2018", "Male"));
+  team.push(new student("Lemmy", "Seia", "1999/02/20", "Optometry", "2020", "Male"));
+  team.push(new student("Nadah", "Ledeigh", "1995/01/09", "Gender Studies", "2018", "Male"));
+  team.push(new student("Bart", "Hoffman", "1996/04/19", "Chemistry", "2019", "Male"));
+  team.push(new student("Henry", "Mao", "1997/08/05", "Chinese History", "2019", "Male"));
+  team.push(new student("Rich", "Alpert", "1999/09/23", "Religion", "2020", "Male"));
 }
